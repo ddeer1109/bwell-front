1f31fce0234848418041cf5eb8ee3d6c531cc5f6
Daniel Wątor [KRK NOV 2020] <dmwator11@gmail.com>
2023-03-05 12:42:12 +0100

    elasticization

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -6,7 +6,7 @@ import TextAreaInput from '../components/entrycreator/inputareas/TextAreaInput';
 import IngredientsList from '../components/entrycreator/inputareas/IngredientsList';
 import { API_BASE_URL } from '../oauth2/constants';
 
-const isLocalhost = () => Boolean(
+const isLocalhost = Boolean(
     window.location.hostname === 'localhost' ||
       // [::1] is the IPv6 localhost address.
       window.location.hostname === '[::1]' ||
@@ -14,7 +14,7 @@ const isLocalhost = () => Boolean(
       window.location.hostname.match(
         /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
       )
-);
+  );
 const APIhost = API_BASE_URL + "/api/v1"
 
 
